e9fa615ac7a3841a2e58b07bfd688052341cf388
HYDR00GEN <ovy.hydrogen@gmail.com>
2022-09-12 08:18:32 +0200

    adding C00

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: opaunas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/10 18:18:39 by opaunas           #+#    #+#             */
+/*   Updated: 2022/09/11 17:50:22 by opaunas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_write(int a, int b)
+{
+	if (b != 1)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+	ft_putchar(48 + a / 10);
+	ft_putchar(48 + a % 10);
+	ft_putchar(' ');
+	ft_putchar(48 + b / 10);
+	ft_putchar(48 + b % 10);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_write(a, b);
+		b++;
+		}
+	a++;
+	}
+}
